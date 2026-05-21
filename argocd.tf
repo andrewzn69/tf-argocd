9d18aa4c0f989c8ad2974acbcad3499ef56662af
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-05-21 12:45:28 +0200

    add missing argocd_version

--- argocd.tf
+++ argocd.tf
@@ -26,6 +26,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
+  version          = var.argocd_version
   namespace        = "argocd"
   create_namespace = true
   wait             = true
